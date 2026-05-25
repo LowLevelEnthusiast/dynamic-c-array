ab65ceb419f583e7bced2cf994a3ced390babad6
LowLevelEnthusiast <157925714+LowLevelEnthusiast@users.noreply.github.com>
2026-05-25 21:21:06 +0530

    Initial commit

---
+++ dynamic_arrays.c
@@ -0,0 +1,48 @@
+#include "dynamic_arrays.h"
+void *vector_create(size_t el_size) {
+  size_t initial_size = 10;
+  metadata *header = malloc(sizeof(metadata) + initial_size * el_size);
+  if (header == NULL) {
+    printf("Issues in allocation of memory\n");
+    exit(1);
+  }
+  header->size = 0;
+  header->capacity = initial_size;
+  header->element_size = el_size;
+  return (void *)(header + 1);
+}
+void *vector_create_with_pre_allocation(size_t el_size, size_t num) {
+  size_t initial_size = num;
+  metadata *header = malloc(sizeof(metadata) + initial_size * el_size);
+  if (header == NULL) {
+    printf("Issues in allocation of memory\n");
+    exit(1);
+  }
+  header->size = 0;
+  header->capacity = initial_size;
+  header->element_size = el_size;
+  return (void *)(header + 1);
+}
+void increase_size_push_back(void **arr) {
+  metadata *header = (metadata *)(*arr) - 1;
+  if (header->capacity == header->size) {
+    header->capacity *= 2;
+    header = realloc(header, header->capacity * header->element_size +
+                                 sizeof(metadata));
+    if (header == NULL) {
+      printf("Issues in reallocation of storage\n");
+      exit(1);
+    }
+    (*arr) = (void *)(header + 1);
+  }
+}
+
+void _free_vector(void **arr) {
+  metadata *header = (metadata *)(*arr) - 1;
+  free(header);
+}
+
+void _reuse_vector(void **arr) {
+  metadata *header = (metadata *)(*arr) - 1;
+  header->size = 0;
+}
---
+++ dynamic_arrays.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct {
+  size_t capacity;
+  size_t size;
+  size_t element_size;
+} metadata;
+#define header(s) (get_header(&s))
+#define get_header(s) ((metadata *)(*s) - 1)
+#define push_back(arr, v)                                                      \
+  do {                                                                         \
+    if (is_full(arr)) {                                                        \
+      increase_size_push_back((void **)&(arr));                                \
+    }                                                                          \
+    (arr)[get_vector_size(arr)] = v;                                           \
+    metadata *header = (metadata *)((char *)(arr) - sizeof(metadata));         \
+    header->size++;                                                            \
+  } while (0)
+#define is_full(s) ((header(s)->size == header(s)->capacity ? 1 : 0))
+#define vector_print(s, id)                                                    \
+  do {                                                                         \
+    metadata *header = header(s);                                              \
+    if (header->size == 0) {                                                   \
+      printf("{}\n");                                                          \
+    } else {                                                                   \
+      printf("{");                                                             \
+      for (int i = 0; i < header->size - 1; i++) {                             \
+        printf(id, s[i]);                                                      \
+        printf(",");                                                           \
+      }                                                                        \
+      printf(id, s[header->size - 1]);                                         \
+      printf("}\n");                                                           \
+    }                                                                          \
+  } while (0)
+#define generate_vector(s) (vector_create(sizeof(s)))
+#define get_vector_size(s) ((header(s))->size)
+#define get_vector_capacity(s) ((header(s))->capacity)
+#define free_vector(s) (_free_vector(&s))
+#define reuse_vector(s) (_reuse_vector((void **)&s))
+#define pre_allocation_vector_generation(type, num)                            \
+  (vector_create_with_pre_allocation(sizeof(type), num))
+#define remove_element(arr, index)                                             \
+  do {                                                                         \
+    metadata *header = (metadata *)((char *)(arr) - sizeof(metadata));         \
+    if ((index) >= header->size) {                                             \
+      printf("index out of bounds\n");                                         \
+    } else {                                                                   \
+      for (int i = index; i < header->size - 1; i++) {                         \
+        arr[i] = arr[i + 1];                                                   \
+      }                                                                        \
+      header->size--;                                                          \
+      if (header->size < (header->capacity) / 4 && header->size > 10) {        \
+        (header->capacity) /= 2;                                               \
+        header = realloc(header, header->capacity * header->element_size +     \
+                                     sizeof(metadata));                        \
+        if (header == NULL) {                                                  \
+          printf("Issues in reallocation of storage\n");                       \
+          exit(1);                                                             \
+        }                                                                      \
+      }                                                                        \
+    }                                                                          \
+    arr = (void *)(header + 1);                                                \
+  } while (0)
+#define GET_MACRO(a, b, NAME, ...) NAME
+#define create_vector(...)                                                     \
+  GET_MACRO(__VA_ARGS__, pre_allocation_vector_generation,                     \
+            generate_vector)(__VA_ARGS__)
+void *vector_create(size_t el_size);
+void increase_size_push_back(void **arr);
+void *vector_create_with_pre_allocation(size_t el_size, size_t num);
+void _free_vector(void **arr);
+void _reuse_vector(void **arr);
